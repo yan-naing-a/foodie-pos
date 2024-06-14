a615ed344294b8bc1b52a6c67396e4ee12885ed3
Yan Naing Aung <yannaingaung2209@gmail.com>
2024-06-14 15:24:27 +0630

    init

---
+++ src/pages/backoffice/index.tsx
@@ -0,0 +1,10 @@
+import { Box, Typography } from "@mui/material";
+export default function () {
+  return (
+    <Box>
+      <Box>
+        <Typography variant="h3">backoffice app</Typography>
+      </Box>
+    </Box>
+  );
+}
